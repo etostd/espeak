03b78305a4a00b3d113dd9f1d4269d8f0cee93fe
Steffen Dalgard <steffen.dalgard@sintef.no>
2016-05-15 15:58:05 +0200

    Added redirect in script

--- espeak-1.48.04-source/my_test/speak_from_fifo.sh
+++ espeak-1.48.04-source/my_test/speak_from_fifo.sh
@@ -3,7 +3,7 @@
 c=1
 while [ $c -gt 0 ] 
 do
-  echo "Run $c ..."
+  echo "Run $c ..." > /var/log/lirctmp/espeak.out
   (( c++ ))
-  ../src/espeak -vno -p 110 -s 100 < textfifo 
+  ../src/espeak -vno -p 110 -s 100 < textfifo  >> /var/log/lirctmp/espeak.out 2>&1
 done
